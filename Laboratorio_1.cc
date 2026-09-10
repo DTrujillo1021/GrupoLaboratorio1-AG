bca9e3447d13a1ecf3ac2e4efe35833f25368782
Diego Roberto Hernandez Trujillo <00094726@uca.edu.sv>
2026-09-10 14:29:54 -0600

    Funcion calcular calorias

--- Laboratorio_1.cc
+++ Laboratorio_1.cc
@@ -1,5 +1,16 @@
-#include <iostrean>
+#include <iostream>
+
+float ConsultarCalorias(float calorias);
 
 int main(){
+    std::cout << "carga inicial" << std:: endl;
+    ConsultarCalorias(0);
     
+
+
+}
+
+float ConsultarCalorias(float calorias)
+{
+    std::cout << "Calorias actuales: "<< calorias << "Cal."<< std:: endl;
 }
