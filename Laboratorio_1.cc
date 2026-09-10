95647518a0c369123e6891fc6e717d35584b6439
CesarLandaverdeuca <00057426@uca.edu.sv>
2026-09-10 14:25:10 -0600

    se acabo la funcion quemar calorias

--- Laboratorio_1.cc
+++ Laboratorio_1.cc
@@ -1,5 +1,42 @@
-#include <iostrean>
+#include <iostream>
+
+struct Calorias
+{
+    int calorias;
+};
+
+void QuemarCalorias(int *calorias, int cant);
 
 int main(){
+    Calorias calorias;
+
+    calorias.calorias=2000;
+
+
+ QuemarCalorias(&calorias.calorias, 1000);
+
+
+ std::cout<<"Despues de consumir 1000 calorias las calorias son:"<<std::endl;
+ 
+
+}
+
+
+void QuemarCalorias(int *calorias, int cal)
+{
+   //no se puede restar calorias negativas:
+    if (*calorias <= 0)
+    {
+        std::cout<<"NO se puede quemar calorias negativas :)"<<std::endl;
+    }
+
+    //no puede las calorias menor a 0
+     else if (*calorias==0)
+     {
+        std::cout<<"No se puede quemar calorias ya se consumieron todas :("<<std::endl;
+
+     }
+    else {*calorias -= cal;}
+
     
 }
