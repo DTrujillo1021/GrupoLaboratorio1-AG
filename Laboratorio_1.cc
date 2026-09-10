5dc7f22433a196aa169cec03211cc243c58e2ea8
Darwin Gabourel <00101726@uca.edu.sv>
2026-09-10 14:32:18 -0600

    funcionagregarcalorias

--- Laboratorio_1.cc
+++ Laboratorio_1.cc
@@ -1,5 +1,23 @@
-#include <iostrean>
+#include <iostream>
+
+void agregarCalorias(int &calorias, int calo);
 
 int main(){
-    
+    int calorias = 2000;
+    int calo;
+std :: cout << "ingrese las calorias cosumidas: " << std :: endl;
+std :: cin >> calo;
+    agregarCalorias(calorias, calo);
+    std::cout << "despues de consumir esas calorias " << calorias << std::endl;
+
+    return 0;
+}
+
+void agregarCalorias(int &calorias, int calo){
+    if (calo < 0){
+        std :: cout << "error: las calorias consumidas no pueden ser negativas" << std :: endl;
+        std :: cout << "tus calorias siguen siendo: " << calorias << std :: endl;
+        return;
+    }
+    calorias += calo;
 }
